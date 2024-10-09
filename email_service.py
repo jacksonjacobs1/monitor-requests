4d69250aa62dc25a3106d93135de246314fae046
Jackson <jacksonjjacobs@gmail.com>
2024-10-09 18:35:45 -0400

    done

--- email_service.py
+++ email_service.py
@@ -5,13 +5,16 @@ import time
 import argparse
 
 if __name__ == "__main__":
-    to_email = "nli31@emory.edu"
+    to_email = "jjaco34@emory.edu"
     log_path = "app.log"
 
     while True:
-        time.sleep(1000)
+        print("Starting the check")
         now = time.time()
         latest_log = os.path.getmtime(log_path)
 
         if now - latest_log > 300:
             os.system(f"echo 'The log file has not been updated in the last 5 minutes' | mail -s 'Log file not updated' {to_email}")
+            print("Email sent")
+
+        time.sleep(1000)
