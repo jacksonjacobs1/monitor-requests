9baeb699f47fdd11945d85c30dd238406c904939
Jackson <jacksonjjacobs@gmail.com>
2024-10-10 10:47:09 -0400

    updated poll interval

--- email_service.py
+++ email_service.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     log_path = "app.log"
 
     while True:
-        print("Starting the check")
+        print("Polling log file")
         now = time.time()
         latest_log = os.path.getmtime(log_path)
 
-        if now - latest_log > 300:
+        if now - latest_log > 600:
             os.system(f"echo 'The log file has not been updated in the last 5 minutes' | mail -s 'Log file not updated' {to_email}")
             print("Email sent")
 
-        time.sleep(1000)
+        time.sleep(300)
